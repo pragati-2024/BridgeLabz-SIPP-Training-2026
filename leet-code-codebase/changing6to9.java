e5eef7b3072602f466e4a63a2de82ef923ac4395
pragati-2024 <bpragati21@gmail.com>
2026-06-09 10:51:02 +0530

    [Pragati bansal] added:programming-elements include classe object static variabe,method and all ...adding control flow like if else and switch case

---
+++ leet-code-codebase/changing6to9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class changing6to9{
+    public static void main(String[] args) {
+        //input = 9996
+        //output = 9999
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+}
