5a2b569fcbdbfdaad0ed7c003cf3feda82b4c95e
pragati-2024 <bpragati21@gmail.com>
2026-06-09 12:55:35 +0530

    [PRAGATI BANSAL] ADD: finding ugly number

--- core-java-practicee/leet-code-codebasee/changing6to9.java
+++ core-java-practicee/leet-code-codebasee/changing6to9.java
@@ -4,8 +4,8 @@ class changing6to9 {
         int temp = num;
         int position = 0;
         int changePos = -1;
-        //sbse phle temp mae store kar hae
-        //storing position of the value
+
+        // 6 ki position find karna
         while (temp > 0) {
             if (temp % 10 == 6) {
                 changePos = position;
@@ -13,12 +13,22 @@ class changing6to9 {
             temp /= 10;
             position++;
         }
+
+        // 6 ko 9 me convert karna
         if (changePos != -1) {
-            num += 3 * (int)Math.pow(10, changePos);
+            num += 3 * (int) Math.pow(10, changePos);
         }
+
         return num;
     }
+
     public static void main(String[] args) {
-        
+        changing6to9 obj = new changing6to9();
+
+        int num = 9669;
+        int result = obj.maximum69Number(num);
+
+        System.out.println("Original Number: " + num);
+        System.out.println("Maximum Number: " + result);
     }
 }
