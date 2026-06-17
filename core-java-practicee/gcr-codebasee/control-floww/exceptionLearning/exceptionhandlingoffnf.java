74139ef895998b9d8d5a40f676462fadce9aa672
pragati-2024 <bpragati21@gmail.com>
2026-06-17 09:59:35 +0530

    [PRAGATI BANSAL] ADD:EXCEPTION LEARNING->ARITHMETIC,NULLPOINTER,FNF

--- core-java-practicee/gcr-codebasee/control-floww/exceptionLearning/exceptionhandlingoffnf.java
+++ core-java-practicee/gcr-codebasee/control-floww/exceptionLearning/exceptionhandlingoffnf.java
@@ -1,9 +1,14 @@
+import java.io.FileNotFoundException;
 public class exceptionhandlingoffnf {
-    static void method1(){
-        exceptionfilenotfound.calculateTotal();
+    static void method1() {
+        try {
+            exceptionfilenotfound.calculateTotal();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     public static void main(String[] args) {
         method1();
     }
 }
-
