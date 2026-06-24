d1c251e724329440d9d7c256cbba616d31119cdf
pragati-2024 <bpragati21@gmail.com>
2026-06-24 10:29:07 +0530

    [PRAGATI BANSAL] ADD: implement smart home, course hierarchy and restaurant management inheritance programs

---
+++ core-java-practicee/gcr-codebasee/programming-elementss/Scenerio/inheritance-scenerio/RestaurantManagementSystem.java
@@ -0,0 +1,60 @@
+class Person {
+
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+interface Worker {
+
+    void performDuties();
+}
+
+class Chef extends Person
+        implements Worker {
+
+    Chef(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+
+        System.out.println(
+                name + " is preparing food.");
+    }
+}
+
+class Waiter extends Person
+        implements Worker {
+
+    Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+
+        System.out.println(
+                name + " is serving customers.");
+    }
+}
+
+public class RestaurantManagementSystem {
+
+    public static void main(String[] args) {
+
+        Chef chef =
+                new Chef("Rahul", 101);
+
+        Waiter waiter =
+                new Waiter("Aman", 201);
+
+        chef.performDuties();
+        waiter.performDuties();
+    }
+}
