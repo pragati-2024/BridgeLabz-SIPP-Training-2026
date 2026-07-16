bfcbd1f01dac0302a0ca95262a426be6b7deb55b
Pragati Bansal <bpragati21@gmail.com>
2026-07-16 12:29:34 -0700

    [PRAGATI BANSAL] ADD:SAVING ALL THE FILES

--- core-java-practicee/gcr-codebasee/control-floww/Linkedlistfolder/main.java
+++ core-java-practicee/gcr-codebasee/control-floww/Linkedlistfolder/main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        linkedlistlearning<Integer> list = new linkedlistlearning<>();
+        LinkedListLearning<Integer> list = new LinkedListLearning<>();
         list.addFirst(10);
         list.addFirst(20);
         list.addFirst(30);
@@ -12,7 +12,7 @@ class Main {
         int rem2 = list.removeLast();
         System.out.println("element removed from last: " + rem2);
 
-        Node<Integer> searched = list.search(40);
+        Node searched = list.search(40);
         System.out.println(searched != null ? "SEARCHED:- " + searched.getData() : "SEARCHED:- not found");
         boolean isInserted = list.insert(70,10);
         System.out.println("inserted:- "+isInserted);
